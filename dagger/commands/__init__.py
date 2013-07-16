f47952c329792fd95a2b81372e39714279e6a770
Austin Marshall <amarshall@numenta.com>
2013-07-16 15:33:15 -0700

    Remove dep command

--- dagger/commands/__init__.py
+++ dagger/commands/__init__.py
@@ -1,6 +1,5 @@
 __all__ = \
   [
-    'dep',
     'init',
     'run',
     'status'
--- dagger/commands/run.py
+++ dagger/commands/run.py
@@ -1,10 +1,15 @@
 import sys
 
 from optparse import OptionParser
+from subprocess import call
 
 import dagger
 
 parser = OptionParser(usage='%%prog %s [PATH]' % __name__.rpartition('.')[2])
+parser.add_option('-b', '--branch',
+  dest='branch',
+  default='CI',
+  help='Stable branch name [Default: CI]')
 
 def handle(options, args):
   """ Every dagger has a handle! """
@@ -13,3 +18,21 @@ def handle(options, args):
     path = args.pop(0)
   except IndexError:
     path = '.'
+
+  with dagger.workingdir(path):
+    lastbranch = dagger.getcwdbranch()
+
+    # Checkout working branch
+    if call(['git checkout %s' % options.branch], shell=True):
+      """ Non-zero return status """
+      return
+
+    config = dagger.Config()
+    tracking = config.get('dagger', 'tracking')
+    revisions = dagger.getrevisions(options.branch, tracking)
+    print options.branch, 'is', len(revisions), 'revisions behind', tracking
+
+    call(['git merge --no-ff %s -m "%s"' % (tracking, "Merging master...")], shell=True)
+
+    # Return to original branch
+    call(['git checkout %s' % lastbranch], shell=True)
